807dae5d9d8cbcc205740e42faccac18566b9212
lpelczar <lukepelczar@gmail.com>
2018-08-02 14:33:57 +0200

    Implement main activity

--- app/src/main/java/com/udacity/lukasz/stocktracker/MainActivity.java
+++ app/src/main/java/com/udacity/lukasz/stocktracker/MainActivity.java
@@ -2,12 +2,44 @@ package com.udacity.lukasz.stocktracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.udacity.lukasz.stocktracker.fragment.StockFragment;
+import com.udacity.lukasz.stocktracker.model.Stock;
+
+public class MainActivity extends AppCompatActivity implements
+        StockFragment.OnStockFragmentInteractionListener  {
+
+    public static final String ARG_STOCK_FRAGMENT = "stock-fragment";
+    private StockFragment stockFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            stockFragment = (StockFragment) getSupportFragmentManager()
+                    .getFragment(savedInstanceState, ARG_STOCK_FRAGMENT);
+        }
+
+        getStocksFromApi();
+    }
+
+    private void getStocksFromApi() {
+
+    }
+
+    @Override
+    public void onStockItemInteraction(Stock stock) {
+        Toast.makeText(this, "Stock: " + stock, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (stockFragment != null && stockFragment.isAdded()) {
+            getSupportFragmentManager().putFragment(outState, ARG_STOCK_FRAGMENT, stockFragment);
+        }
+        super.onSaveInstanceState(outState);
     }
 }
