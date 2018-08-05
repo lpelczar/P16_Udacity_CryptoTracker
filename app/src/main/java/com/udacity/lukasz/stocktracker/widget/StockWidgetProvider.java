dfab49b46bf0cf3d5dfb0463b59069f4e69e01e2
lpelczar <lukepelczar@gmail.com>
2018-08-05 23:20:33 +0200

    Add stock widget provider

---
+++ app/src/main/java/com/udacity/lukasz/stocktracker/widget/StockWidgetProvider.java
@@ -0,0 +1,56 @@
+package com.udacity.lukasz.stocktracker.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.RemoteViews;
+
+import com.google.gson.Gson;
+import com.udacity.lukasz.stocktracker.MainActivity;
+import com.udacity.lukasz.stocktracker.R;
+import com.udacity.lukasz.stocktracker.model.Stock;
+
+import java.util.Locale;
+
+public class StockWidgetProvider extends AppWidgetProvider {
+
+    public static String SHARED_PREFERENCES = "StockWidgetData";
+    public static String KEY_STOCK = "Stock";
+
+    public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                       int appWidgetId, Stock stock) {
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_stock);
+        views.setViewVisibility(R.id.widget_starter_text, View.GONE);
+        views.setViewVisibility(R.id.widget_starter_image, View.GONE);
+
+        views.setTextViewText(R.id.price_tv, String.format(Locale.getDefault(), "%.2f", stock.getPrice()));
+        views.setTextViewText(R.id.high_tv, String.format(Locale.getDefault(), "%.2f", stock.getHigh()));
+        views.setTextViewText(R.id.low_tv, String.format(Locale.getDefault(), "%.2f", stock.getLow()));
+        views.setTextViewText(R.id.open_tv, String.format(Locale.getDefault(), "%.2f", stock.getOpen()));
+        views.setTextViewText(R.id.change24_tv, String.format(Locale.getDefault(), "%.2f", stock.getChange24h()));
+        views.setTextViewText(R.id.change_percent_tv, String.format(Locale.getDefault(), "%.2f", stock.getChange24hPercent()));
+        views.setTextViewText(R.id.volume_24h_tv, String.format(Locale.getDefault(), "%.2f", stock.getVolume24h()));
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE);
+        String stockJson = sharedPreferences.getString(KEY_STOCK, null);
+        Stock stock = new Gson().fromJson(stockJson, Stock.class);
+        if (stock != null) {
+            for (int appWidgetId : appWidgetIds) {
+                updateAppWidget(context, appWidgetManager, appWidgetId, stock);
+            }
+        }
+    }
+}
